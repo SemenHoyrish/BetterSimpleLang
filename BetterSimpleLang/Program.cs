aff6617b88d7b4d96b6dcdf8eed86b093241516a
SemenHoyrish <semushka.2016@gmail.com>
2022-07-11 14:31:14 +0500

    Добавьте файлы проекта.

---
+++ BetterSimpleLang/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BetterSimpleLang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
